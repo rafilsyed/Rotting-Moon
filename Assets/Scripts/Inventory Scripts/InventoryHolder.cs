8036d073b6c0302d1b87e463635633980a286352
Maxence <169144059+maxenceb91@users.noreply.github.com>
2025-11-26 16:19:51 +0100

    Ajouter d'un système simple d'inventaire avec des slots et des items récupérable sur le sol

---
+++ Assets/Scripts/Inventory Scripts/InventoryHolder.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InventoryHolder : MonoBehaviour
+{
+    [SerializeField] private int inventorySize;
+    [SerializeField] protected InventorySystem inventorySystem;
+
+    public InventorySystem InventorySystem => inventorySystem;
+
+    public static UnityAction<InventorySystem> OnDynamicInventoryDisplayRequested;
+
+    private void Awake()
+    {
+        inventorySystem = new InventorySystem(inventorySize);
+    }
+}
